9803fd14a64581436915ca234f328911f4209b2d
Lucas <lucasapessoa@outlook.com>
2024-11-08 12:45:46 -0300

    Add task adition functionality

--- src/models/TaskManager.ts
+++ src/models/TaskManager.ts
@@ -2,6 +2,17 @@ import { Task } from "../interfaces/Task";
 
 let date = new Date()
 export class TaskManager {
-  
+    private tasks: Task[] = [];
+
+    addNewTask(title: string){
+        const newTask: Task = {
+            id: this.tasks.length + 1,
+            title: title,
+            date: date,
+            completed: false
+        }
+        this.tasks.push(newTask)
+    }
+
 
 }

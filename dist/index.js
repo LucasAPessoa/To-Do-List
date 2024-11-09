4cb201dff522bc7b32e8dd3bc15be10c28da93a8
Lucas <lucasapessoa@outlook.com>
2024-11-09 13:14:00 -0300

    Compile test

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
-export {};
+import { TaskManager } from "./models/TaskManager.js";
+
+const taskManager = new TaskManager();
+
+const btn = document.getElementById("btn");
+const title = document.getElementById("title");
+
+function addNewTask() {
+    taskManager.addNewTask(title.innerText);
+}
+if (btn) {
+    btn.addEventListener("click", () => {
+        console.log("Button clicked!");
+    });
+}

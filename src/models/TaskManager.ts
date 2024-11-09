fc8523291e75926c9d5b45545da4639904af48b2
Lucas <lucasapessoa@outlook.com>
2024-11-08 22:27:51 -0300

    Add getTasks and deleteTask methods

--- src/models/TaskManager.ts
+++ src/models/TaskManager.ts
@@ -1,18 +1,29 @@
 import { Task } from "../interfaces/Task";
 
-let date = new Date()
+let date = new Date();
 export class TaskManager {
-    private tasks: Task[] = [];
+  private tasks: Task[] = [];
 
-    addNewTask(title: string){
-        const newTask: Task = {
-            id: this.tasks.length + 1,
-            title: title,
-            date: date,
-            completed: false
-        }
-        this.tasks.push(newTask)
+  addNewTask(title: string) {
+    const newTask: Task = {
+      id: this.tasks.length + 1,
+      title: title,
+      date: date,
+      completed: false,
+    };
+    this.tasks.push(newTask);
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  deleteTask(id: number): boolean {
+    const taskIndex = this.tasks.findIndex((task) => task.id === id);
+    if (taskIndex !== -1) {
+      this.tasks.splice(taskIndex, 1);
+      return true;
     }
-
-
+    return false;
+  }
 }

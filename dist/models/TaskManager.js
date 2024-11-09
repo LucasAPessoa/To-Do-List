b92c2989dc24a0a53c726116390c726a2c0f54fa
Lucas <lucasapessoa@outlook.com>
2024-11-08 22:40:28 -0300

    Use npx tsc to compile TypeScript files to JavaScript for testing logic in HTML

--- dist/models/TaskManager.js
+++ dist/models/TaskManager.js
@@ -1,5 +1,4 @@
 let date = new Date();
-
 export class TaskManager {
     constructor() {
         this.tasks = [];
@@ -9,8 +8,35 @@ export class TaskManager {
             id: this.tasks.length + 1,
             title: title,
             date: date,
-            completed: false
+            completed: false,
         };
         this.tasks.push(newTask);
+    }
+    getTasks() {
+        return this.tasks;
+    }
+    deleteTask(id) {
+        const taskIndex = this.tasks.findIndex((task) => task.id === id);
+        if (taskIndex !== -1) {
+            this.tasks.splice(taskIndex, 1);
+            return true;
+        }
+        return false;
+    }
+    completeTask(id) {
+        const selectedTask = this.tasks.find((task) => task.id === id);
+        if (selectedTask) {
+            selectedTask.completed = true;
+            return true;
+        }
+        return false;
+    }
+    uncompleteTask(id) {
+        const selectedTask = this.tasks.find((task) => task.id === id);
+        if (selectedTask) {
+            selectedTask.completed = false;
+            return false;
+        }
+        return true;
     }
 }

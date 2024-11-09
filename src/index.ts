f3cccbaa6228933e241bbe8fed8ba0e130a2eeb8
Lucas <lucasapessoa@outlook.com>
2024-11-09 13:10:47 -0300

    Add logic for html elements to test taskManager methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 import { TaskManager } from "./models/TaskManager";
+
+const taskManager = new TaskManager()
+
+const btn: HTMLElement = document.getElementById("btn")!
+const title: HTMLElement = document.getElementById("title")!
+
+function addNewTask(){
+   
+    taskManager.addNewTask(title.innerText)
+  
+}
+
+if (btn) {
+    btn.addEventListener("click", () => {
+      console.log("Button clicked!");
+    });
+  }

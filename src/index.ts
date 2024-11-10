1b67d0a892ea51ff62f922fc1a7f3332a8729e36
Lucas <lucasapessoa@outlook.com>
2024-11-10 12:51:37 -0300

    Add comments to improve code clarity

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,22 @@ const taskManager = new TaskManager()
 const addBtn: HTMLElement = document.getElementById("btn")!
 const title = document.getElementById("title")! as HTMLInputElement
 
+//Function to add a new task
 function addNewTask(){
 
+  //Verify if the value of title is null
   if(title.value === ""){
     console.log("É preciso adicionar um título")
-  }else{    
+  }
+  //If title is not null, add the new task and show all tasks in console
+  else{    
     taskManager.addNewTask(title.value);
     console.log(taskManager.getTasks());
   }
   
 }
 
-
-
+//Add a click event for the add a task button
 if (addBtn) {
     addBtn.addEventListener("click", () => addNewTask());
   }
